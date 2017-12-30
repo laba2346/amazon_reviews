fc6493ce4bbca3b4ae073ec244a4bfb82b1667e3
Landon <laba2346@colorado.edu>
2017-12-29 17:49:52 -0700

    added a test that verifies that the csv was written correctly

--- test/parseCSVTest.py
+++ test/parseCSVTest.py
@@ -9,7 +9,7 @@ from word import Word
 
 class TestParseCSV(unittest.TestCase):
     def test_first_row(self):
-        os.chdir("..")
+        os.chdir("/Users/landon/Dropbox/Amazon project/")
         expRow = []
         #Get current first row, then append to it the expected columns
         with open("example2.csv", 'r') as csvInput:
@@ -35,6 +35,41 @@ class TestParseCSV(unittest.TestCase):
         parseCSV("example2.csv")
         with open("out.csv", 'r') as csvOutput:
             reader = csv.reader(csvOutput, delimiter=',', quotechar='"')
+            gotRow = next(reader)
+            self.assertEqual(expRow, gotRow)
+
+    #Once a review is scored, its data is written to a CSV. This tests the
+    #output of a single review.
+    def test_second_row(self):
+        os.chdir("/Users/landon/Dropbox/Amazon project/")
+        expRow = []
+        #Get the current row for the first review before we append to it
+        with open("example2.csv", 'r') as csvInput:
+            reader = csv.reader(csvInput, delimiter=",", quotechar='"')
+            row0 = next(reader)
+            expRow = next(reader)
+            csvInput.seek(0)
+        expRow.append("63")
+        expRow.append("48")
+        expRow.append("17")
+        expRow.append("72161")
+        expRow.append("145")
+        expRow.append("1569")
+        expRow.append("254340")
+        expRow.append("7531")
+        expRow.append("5021")
+        expRow.append("3903")
+        expRow.append("4088")
+        expRow.append("3534")
+        expRow.append("688")
+        expRow.append("614")
+
+        #parse the csv and produce the "got" output
+        parseCSV("example2.csv")
+
+        with open("out.csv", 'r') as csvOutput:
+            reader = csv.reader(csvOutput, delimiter=',', quotechar='"')
+            row0 = next(reader)
             gotRow = next(reader)
             self.assertEqual(expRow, gotRow)
 
